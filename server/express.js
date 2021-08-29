b215a48924bc2b334213a7687f97fe23e723d3c7
Gina Zampino <zampino.gina@gmail.com>
2021-08-28 20:02:45 -0400

    Consolidated server.

--- server/express.js
+++ server/express.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
 
+/* =============================================================
+
+    Packages
+
+   ============================================================= */
+
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
@@ -7,10 +13,14 @@ const passport = require('passport');
 const app = express();
 const { Model } = require('objection');
 const Knex = require('knex');
+const router = express.Router();
 
-const routes = require('./routes');
+/* =============================================================
 
-app.use(routes);
+    Middleware
+
+   ============================================================= */
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, '../')));
@@ -22,6 +32,12 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+/* =============================================================
+
+    Objection & Knex
+
+   ============================================================= */
 
 const knex = Knex({
     client: 'mysql2',
@@ -42,14 +58,42 @@ class User extends Model {
     };
 };
 
-async function main() {
+async function test() {
     const test = await User.query()
     .where('userEmail', 'seahorror@gmail.com')
     .orderBy('id');
     console.log(test)
 };
 
-main();
+test();
+
+/* =============================================================
+
+    Routes
+
+   ============================================================= */
+
+router.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../pages/index.html'));
+});
+
+router.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../pages/login.html'));
+});
+
+router.post('/debug/login', (req, res) => {
+    
+});
+
+router.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../pages/register.html'));
+});
+
+/* =============================================================
+
+    Listen...
+
+   ============================================================= */
 
 app.listen(process.env.PORT, () => {
     console.log(
